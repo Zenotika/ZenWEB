200cf0420e3b33cec96f034d115581a9d045071b
Farid <riddd29@gmail.com>
2025-06-08 01:19:24 +0700

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body className="bg-gradient-to-b from-white to-gray-100 min-h-screen">
         {children}
       </body>
